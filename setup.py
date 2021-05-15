32606bebbc5f10816c42053683501d2d0d14bbb2
StephenGemin <45926479+StephenGemin@users.noreply.github.com>
2021-05-14 21:51:24 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -17,14 +17,20 @@ setup(
   author='Stephen Gemin',
   author_email='s.gemin88@gmail.com',
   url='https://github.com/StephenGemin/code_timer',
-  download_url='https://github.com/StephenGemin/code_timer/'
-               'archive/refs/tags/1.1.4.tar.gz',
+  download_url='https://github.com/StephenGemin/code_timer/',
+  extras_require={
+          'dev': [
+              'pytest',
+              'pytest-pep8',
+              'pytest-cov'
+          ]
+  },
   keywords=["decorator", "decorators", "code timer", "timing",
             "code timing", "context manager", "profiling"],
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
+    'Topic :: Software Development :: Debuggers',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
