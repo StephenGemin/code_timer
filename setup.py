51c4b22bd3632bf8a713e17d23c108ef1c7f4b4b
Stephen Gemin <45926479+StephenGemin@users.noreply.github.com>
2021-07-31 01:16:25 -0400

    rev-up from 1.1.7 to 2.0.0 (#14)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(current_dir, 'README.md'), encoding='utf-8') as f:
 setup(
   name='code_timer',
   packages=['code_timer'],
-  version='1.1.7',
+  version='2.0.0',
   license='MIT License',
   description='Custom timer for your Python coding pleasure',
   long_description=long_desc,
